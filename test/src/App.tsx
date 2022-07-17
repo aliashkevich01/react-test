b1f6fc6afbd70c877c902a00792b8039f93ff8fe
Andrei <aliashkevich01@gmail.com>
2022-07-17 15:30:46 +0300

    change router for hash-router for deploy

--- test/src/App.tsx
+++ test/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import { AUTH_PATH, MAIN_PATH, USER_PATH } from './constants';
 import Page404 from './pages/404/404';
 import Auth from './pages/auth/Auth';
@@ -8,14 +8,14 @@ import Personal from './pages/Personal/Personal';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path={MAIN_PATH} element={<Main />} />
           <Route path={AUTH_PATH} element={<Auth />} />
           <Route path={USER_PATH} element={<Personal />} />
           <Route path="/:nonfound" element={<Page404 />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
